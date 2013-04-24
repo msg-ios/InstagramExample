4a034730bbad373bdc387f99d8802f045715df1d
msg-ios <msg.maruko@gmail.com>
2013-04-23 23:01:25 -0300

    Initial Commit

---
+++ InstagramExample/InstagramAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  InstagramAppDelegate.h
+//  InstagramExample
+//
+//  Created by Marco S. Graciano on 4/23/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InstagramAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InstagramExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InstagramExample
+//
+//  Created by Marco S. Graciano on 4/23/13.
+//  Copyright (c) 2013 Marco Graciano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "InstagramAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([InstagramAppDelegate class]));
+    }
+}
